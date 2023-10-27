bd9de10c9e9cdfaae3498ee03f2c770d651de140
Ole <oleh.kodehode@gmail.com>
2023-10-27 17:53:37 +0200

    made reuseable header component

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+import styles from "@/styles/header.module.css";
+
+const Header = ({ title, subtitle, description }) => {
+  return (
+    <header>
+      <h1 className={styles.headerTitle}>{title}</h1>
+      <div className={styles.headerBox}>
+        {<h2 className={styles.headerSubtitle}>{subtitle}</h2>}
+        {description && <p className={styles.headerText}>{description}</p>}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
